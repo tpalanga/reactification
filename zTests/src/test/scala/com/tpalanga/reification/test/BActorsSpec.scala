ac3f1f17b7671e9e0d46788dc3c545f960cb641a
Tudor Palanga <tudor.palanga@gmail.com>
2018-04-16 18:09:10 +0200

    Add tests for actors and rest implementations

---
+++ zTests/src/test/scala/com/tpalanga/reification/test/BActorsSpec.scala
@@ -0,0 +1,52 @@
+package com.tpalanga.reification.test
+
+import com.softwaremill.sttp.akkahttp.AkkaHttpBackend
+import com.softwaremill.sttp.json4s._
+import com.softwaremill.sttp._
+import org.scalatest.{AsyncFlatSpec, EitherValues, Matchers}
+
+object BActorsSpec {
+  val eventId = "event1234"
+  val baseUrl = s"http://localhost:8082/events/tickets/$eventId"
+}
+
+class BActorsSpec extends AsyncFlatSpec with Matchers with EitherValues {
+
+  import BActorsSpec._
+  import ZApiProtocol._
+
+  implicit val backend = AkkaHttpBackend()
+
+  "ActorsService" should "process ticket requests and return download link" in {
+
+    val responseAvailabilities = sttp.get(uri"$baseUrl")
+      .response(asJson[TicketAvailabilities])
+    def responseBook(ticketType: Int, quantity: Int) = sttp
+      .body(TicketRequest(ticketType, quantity))
+      .post(uri"$baseUrl/book")
+      .response(asJson[LeaseResponse])
+    def responsePayment(transactionKey: String, leaseId: String) = sttp
+      .body(PaymentConfirmation(transactionKey, leaseId))
+      .post(uri"$baseUrl/paymentDone")
+      .response(asJson[DownloadResponse])
+
+    for {
+      r1 <- responseAvailabilities.send()
+      availabilities = r1.body.right.value
+      _ = availabilities shouldBe TicketAvailabilities(Map("1" -> TicketStock(100), "2" -> TicketStock(200)))
+      r2 <- responseBook(1, 2).send()
+      lease = r2.body.right.value
+      _ = lease shouldBe a[LeaseResponse]
+      leaseId = lease.leaseId
+      _ = println(s"leaseId: $leaseId")
+      r3 <- responsePayment("PayKey00123", leaseId).send()
+      download = r3.body.right.value
+      _ = download.downloadLink shouldBe "/downloads/tickets/000578.pdf"
+    } yield {
+      backend.close()
+      1 shouldBe 1
+    }
+
+  }
+
+}
